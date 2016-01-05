9165136f6d7fc57075ef80c0693eb0a47bd5a23e
Leon Machens <leon@homelike.cc>
2016-01-05 17:00:09 +0100

    travel time

--- client/commute-maps-demo.js
+++ client/commute-maps-demo.js
@@ -27,6 +27,8 @@ Template.mainTemplate.onCreated(function() {
     }
     self.markersSubscription = newMarkersSubscription;
   });
+
+  this.highlightedMarkers = new Meteor.Collection(null);
 });
 
 Template.mainTemplate.helpers({
@@ -64,6 +66,22 @@ Template.mainTemplate.helpers({
       },
       travelModeChanged: function(travelMode) {
         Session.set('logs', 'travelMode: ' + travelMode + '<br>' + Session.get('logs'));
+      },
+      showcaseMarkersInfos: function(id, infos) {
+        var html;
+        if (!infos) {
+          html = '';
+        } else {
+          var icon = '';
+          switch (infos.travelMode) {
+            case 'DRIVING': icon = 'fa fa-car fa-lg'; break;
+            case 'TRANSIT': icon = 'fa fa-train fa-lg'; break;
+            case 'WALKING': icon = 'fa fa-male fa-lg'; break;
+            case 'BICYCLING': icon = 'fa fa-bicycle fa-lg'; break;
+          }
+          html = '<i class="' + icon + '"></i> ' + infos.distance.text + ': ' + infos.duration.text;
+        }
+        $('#' + id).html(html);
       }
     }
   },
@@ -90,31 +108,50 @@ Template.mainTemplate.helpers({
   },
   logs: function() {
     return Session.get('logs');
+  },
+  showcaseMarkers: function() {
+    return Markers.find({}, {limit: 4});
+  },
+  highlightedMarkers: function() {
+    return Template.instance().highlightedMarkers.find();
   }
 });
 
 Template.mainTemplate.events({
   'click .newMarker': function(event) {
-    var r = 3000/111300 // = 3000 meters
-      , y0 = 52.522114
-      , x0 = 13.410197
-      , u = Math.random()
-      , v = Math.random()
-      , w = r * Math.sqrt(u)
-      , t = 2 * Math.PI * v
-      , x = w * Math.cos(t)
-      , y1 = w * Math.sin(t)
-      , x1 = x / Math.cos(y0)
+    var distanceBounds = Session.get('distanceBounds');
+    var position = distanceBounds.$geoWithin.$centerSphere[0];
+    var r = 5000/111300, // = 5000 meters
+      y0 = position[1],
+      x0 = position[0],
+      u = Math.random(),
+      v = Math.random(),
+      w = r * Math.sqrt(u),
+      t = 2 * Math.PI * v,
+      x = w * Math.cos(t),
+      y1 = w * Math.sin(t),
+      x1 = x / Math.cos(y0)
 
 
     // Insert a marker into the collection
-    Markers.insert(coordinatesHelper(y0 + y1, x0 + x1));
+    var coordinates = coordinatesHelper(y0 + y1, x0 + x1);
+    var max = Math.floor(Math.random() * 40 + 1);
+    for (var i=0; i<max; i++) {
+      Markers.insert(coordinates);
+    }
+    Session.set('logs', 'added ' + max + ' markers<br>' + Session.get('logs'));
   },
-
   'click .removeMarker': function(event) {
     // Remove random marker
     var skip = Math.floor(Math.random() * Markers.find().count());
     var random = Markers.findOne({}, { skip: skip });
     Markers.remove(random._id);
+    Session.set('logs', 'removed random marker<br>' + Session.get('logs'));
+  },
+  'mouseenter .markerItem': function(e, t) {
+    t.highlightedMarkers.insert({pairedCoordinates: this.pairedCoordinates});
+  },
+  'mouseleave .markerItem': function(e, t) {
+    t.highlightedMarkers.remove({pairedCoordinates: this.pairedCoordinates});
   }
 });
